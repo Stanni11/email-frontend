a68cb14eff28a20a0a2ddeb34475b0e5bfd58808
Stanni11 <supercoder@SuperCoders-Air.fritz.box>
2022-10-12 15:13:03 +0200

    first commit

---
+++ src/components/Seat.jsx
@@ -0,0 +1,30 @@
+import './Seat.css'
+const Seat = () => {
+    return (
+        <>
+            <div className="header">
+                <h1>Willkommen im Kino</h1>
+            </div>
+            <div className="loge">
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+            <div className="park">
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+            <div className="leinwand">
+                Leinwand
+
+            </div>
+
+
+        </>
+    )
+}
+
+export default Seat
